9698bf05470ad7bd5661b2799c6fc06133232512
Raveendra Inturi <118992612+raviacts035@users.noreply.github.com>
2023-05-27 11:24:05 +0000

    Login and logout Controllers

--- src/controllers/authControl.js
+++ src/controllers/authControl.js
@@ -4,9 +4,15 @@ import asyncHandler from "../service/asyncHandler";
 import User from "../modules/userSchema.js";
 import CustomError from "../utils/CustomError";
 
+export const cookieOptions={
+    expires : new Date(Date.now()+ 3* 24 * 60 * 60 * 1000),
+    httpOnly: true
+}
+
+
 // Controller for sign-up/registration
 
-const signUp= asyncHandler(async (req,res)=>{
+export const signUp= asyncHandler(async (req,res)=>{
 
     //collect user details
     const {name, email, password}=req.body;
@@ -31,6 +37,8 @@ const signUp= asyncHandler(async (req,res)=>{
 
     const token = user.getJwtToken();
 
+    res.cookie("token",token, cookieOptions)
+
     // Sending Back Responce to user, with selected fields only
     res.status(200).json({
         success :true,
@@ -39,5 +47,50 @@ const signUp= asyncHandler(async (req,res)=>{
     })
 })
 
+// Controller for Login user
 
-export default signUp
+export const logIn= asyncHandler(async (req, res)=>{
+    // colect credentials 
+    const {email, password }= req.body;
+    
+    // validation
+    if (!email || !password){
+        throw new CustomError("Please enter Credentials");
+    }
+
+    // Checking if email Matches
+    const user =User.findOne({email}).select("+password")
+    if (!user){
+        throw new CustomError("Invalid credentials!! ", 400)
+    }
+
+    const passMatches=user.comparePassword(password)
+    if (passMatches){
+        const token = user.getJwtToken();
+        user.password=undefined
+
+        // Sending Back Responce to user, with selected fields only
+        res.cookie("token",token, cookieOptions)
+
+        return res.status(200).json({
+            success :true,
+            token,
+            user
+        })
+    }
+    throw new CustomError("Password is Incorrect", 400);
+} )
+
+
+// Logout user
+export const Logout=asyncHandler(async (req, res,)=>{
+    res.cookie("token",null ,{
+        expires : new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success:true,
+        message: "Log Out Sucessful"
+    })
+})
